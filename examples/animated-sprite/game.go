e0677753a259080102c27adb571c971eddae0748
Sergio Vera <svera@adobe.com>
2020-06-02 09:46:46 +0200

    Made collision truly composable both in file and structs

--- examples/animated-sprite/game.go
+++ examples/animated-sprite/game.go
@@ -3,7 +3,7 @@ package main
 import (
 	"bytes"
 	"image/color"
-	"io/ioutil"
+	"io"
 	"os"
 
 	"github.com/faiface/pixel"
@@ -16,28 +16,20 @@ import (
 	"golang.org/x/image/colornames"
 )
 
-// Animation status
-const (
-	idle = iota
-	running
-	jumping
-	falling
-)
-
 const (
 	impulse = 64
 )
 
 type Game struct {
-	phys   *physic.Physics
-	hero   *animation.BoundedAnimation
-	circle *collision.BoundingCircle
-	canvas *pixelgl.Canvas
-	imd    *imdraw.IMDraw
-	levels []level.Level
-	red    *color.RGBA
-	green  *color.RGBA
-	blue   *color.RGBA
+	phys        *physic.Physics
+	hero        *animation.Animation
+	heroBounds  map[string][]collision.Shaper
+	levelBounds map[string][]collision.Shaper
+	circle      *collision.BoundingCircle
+	canvas      *pixelgl.Canvas
+	imd         *imdraw.IMDraw
+	levels      map[string]level.Level
+	debug       *color.RGBA
 }
 
 func NewGame(width, height float64) *Game {
@@ -45,14 +37,29 @@ func NewGame(width, height float64) *Game {
 	if err != nil {
 		panic(err)
 	}
+	var buf bytes.Buffer
+	tee := io.TeeReader(r, &buf)
 
-	hero, err := animation.LoadBoundedAnimation(r, pixel.V(64, 32))
+	hero, err := animation.Deserialize(tee, pixel.V(64, 32))
+	if err != nil {
+		panic(err)
+	}
+	hero.SetCurrentAnim("idle")
+	heroBounds, err := collision.LoadBounds(&buf)
 	if err != nil {
 		panic(err)
 	}
 
-	data, err := ioutil.ReadFile("levels.json")
-	lvl, err := level.Load(bytes.NewReader(data))
+	data, err := os.Open("levels.json")
+	if err != nil {
+		panic(err)
+	}
+	tee = io.TeeReader(data, &buf)
+	lvl, err := level.Deserialize(tee)
+	if err != nil {
+		panic(err)
+	}
+	levelBounds, err := collision.LoadBounds(&buf)
 	if err != nil {
 		panic(err)
 	}
@@ -65,20 +72,17 @@ func NewGame(width, height float64) *Game {
 				Gravity:      112,
 			},
 		),
-		hero:   hero,
-		circle: collision.NewBoundingCircle(0, 40, 18),
+		hero:        hero,
+		heroBounds:  heroBounds,
+		levelBounds: levelBounds,
+		circle:      collision.NewBoundingCircle(0, 40, 18),
 		// Canvas origin of coordinates will be at its center
 		canvas: pixelgl.NewCanvas(pixel.R(-width/2, -height/2, width/2, height/2)),
 		imd:    imdraw.New(nil),
 		levels: lvl,
 	}
 
-	// Canvas origin of coordinates will be at its center
-	g.red = &color.RGBA{255, 0, 0, 16}
-	g.green = &color.RGBA{0, 255, 0, 16}
-	g.blue = &color.RGBA{0, 0, 255, 16}
-
-	g.levels[0].SetDebug(g.imd, g.blue)
+	g.debug = &color.RGBA{0, 0, 255, 16}
 	return &g
 }
 
@@ -87,29 +91,29 @@ func (g *Game) Loop(win *pixelgl.Window, dt float64) (string, error) {
 
 	g.readInput(win, dt)
 	delta := g.phys.Displacement(dt)
-	sol := g.hero.BoundingShape().Resolve(delta, g.levels[0].Layers[0].Bounds...)
+	sol := g.BoundingShape().Resolve(delta, g.levelBounds["level1-background"]...)
 
-	/*
-		if !g.hero.InBoundsX(delta, g.levels[0].Limits) {
-			delta.X = 0
-			g.phys.StopMovingX()
-		}
-	*/
 	g.updatePosition(sol, delta)
 	g.updateAnim(sol)
 
 	g.canvas.Clear(colornames.Skyblue)
-	g.levels[0].Draw(g.canvas)
+	g.levels["level1"].Draw(g.canvas)
+	if g.debug != nil {
+		for _, b := range g.levelBounds["level1-background"] {
+			b.Shape().Draw(g.debug, g.imd, g.canvas)
+		}
+	}
+
 	g.hero.Draw(g.canvas, dt)
-	g.hero.BoundingShape().Draw(g.red, g.imd, g.canvas)
-	g.circle.Draw(g.green, g.imd, g.canvas)
+	g.BoundingShape().Draw(&color.RGBA{255, 0, 0, 16}, g.imd, g.canvas)
+	g.circle.Draw(&color.RGBA{0, 255, 0, 16}, g.imd, g.canvas)
 	g.imd.Draw(g.canvas)
 	g.canvas.Draw(win, pixel.IM.Moved(win.Bounds().Center()).Scaled(win.Bounds().Center(), zoom))
 	return "game", nil
 }
 
 func (g *Game) readInput(win *pixelgl.Window, dt float64) {
-	if win.JustPressed(pixelgl.KeyUp) && g.hero.CurrentAnim() != jumping && g.hero.CurrentAnim() != falling {
+	if win.JustPressed(pixelgl.KeyUp) && g.hero.CurrentAnim() != "jumping" && g.hero.CurrentAnim() != "falling" {
 		g.phys.SetVelocity(physic.AxisY, impulse)
 	} else if win.Pressed(pixelgl.KeyLeft) {
 		g.hero.Dir = physic.DirectionLeft
@@ -140,12 +144,18 @@ func (g *Game) updatePosition(sol collision.Solution, delta pixel.Vec) {
 
 func (g *Game) updateAnim(sol collision.Solution) {
 	if g.phys.Velocity(physic.AxisY) > 0 {
-		g.hero.SetCurrentAnim(jumping)
+		g.hero.SetCurrentAnim("jumping")
 	} else if g.phys.Velocity(physic.AxisY) < 0 && sol.CollisionAxis != collision.AxisY {
-		g.hero.SetCurrentAnim(falling)
+		g.hero.SetCurrentAnim("falling")
 	} else if g.phys.Velocity(physic.AxisX) != 0 {
-		g.hero.SetCurrentAnim(running)
+		g.hero.SetCurrentAnim("running")
 	} else {
-		g.hero.SetCurrentAnim(idle)
+		g.hero.SetCurrentAnim("idle")
 	}
+}
+
+func (g *Game) BoundingShape() collision.Shape {
+	bb := g.heroBounds[g.hero.CurrentAnim()][g.hero.CurrentFrameNumber()]
+	bb.Shape().Align(g.hero.Position)
+	return bb.Shape()
 }
